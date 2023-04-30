ed91b680f53b23489758d9ab62659e831416b022
apariciodevme <apariciodev@proton.me>
2023-04-30 02:20:12 -0300

    first screen

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,11 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
+import { useNavigation } from '@react-navigation/core';
+import React, { useLayoutEffect } from 'react'
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+
+useLayoutEffect(() => {
+  navigation.setOptions({
+    headerShown: false,
+  })
+}, [])
+
   return (
-    <View>
-      <Text className="">HomeScreen</Text>
+    <SafeAreaView>
+      <Text>HomeScreen</Text>
+    {/* Header */}
+    <View  className="flex-row pb-3 items-center mx-4 space-x-2">
+      <Image
+      source={{
+        uri: 'https://links.papareact.com/wru'
+      }}
+      className="h-7 w-7 bg-gray-300 rounded-full p-6"
+      />
+
+      <View>
+        <Text className="font-semibold text-gray-500 text-xs">Deliver now</Text>
+        <Text className="font-semibold text-gray-900 text-lg">Current Location</Text>
+      </View>
+
     </View>
+
+
+    </SafeAreaView>
   )
 }
 
